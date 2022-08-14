13cd12e59688ccfe10d7d20b8b3e51cb0b5105f3
Shi_Sheng <ohayou@shisheng.icu>
2022-08-14 16:45:58 +0800

    i18n: track missed zh-Hans files

---
+++ EnergyStarPlus/Views/SettingsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using EnergyStarPlus.ViewModels;
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace EnergyStarPlus.Views;
+
+// TODO: Set the URL for your privacy policy by updating SettingsPage_PrivacyTermsLink.NavigateUri in Resources.resw.
+public sealed partial class SettingsPage : Page
+{
+    public SettingsViewModel ViewModel
+    {
+        get;
+    }
+
+    public SettingsPage()
+    {
+        ViewModel = App.GetService<SettingsViewModel>();
+        InitializeComponent();
+    }
+}
